206cf110b7a94f497739bb91321dd88abb3000a3
Anderson Alvim de Souza <alvim@PCA0152G.gauge.local>
2020-01-23 09:21:52 -0300

    Fix

---
+++ build/js/js.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    function modalOpen(nameclass) {
+        $('body').find(nameclass).toggleClass('closed');
+    }
+
+    function modalOpen(tab) {
+        $('body').find(tab).toggleClass('closed');
+    }
+
+});
