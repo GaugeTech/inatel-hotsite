9ff2fb87eea85106800da38cf77fded0a8193139
Anderson Alvim de Souza <alvim@PCA0152G.gauge.local>
2020-01-27 17:14:55 -0300

    Cursos

--- build/js/js.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    function modalToggle() {
-        $('body').find('float-inscreva-se').toggleClass('closed');
-        $('body').find('overlay').toggleClass('closed');
-        console.log('teste');
-    }
-}); 
