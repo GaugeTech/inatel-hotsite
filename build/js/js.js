be0850ff4ecfe3973aca4de54002e46da1c6a1a4
Anderson Alvim de Souza <alvim@PCA0152G.gauge.local>
2020-01-30 17:01:10 -0300

    fix

---
+++ build/js/js.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    function modalToggle() {
+        $('body').find('float-inscreva-se').toggleClass('closed');
+        $('body').find('overlay').toggleClass('closed');
+        console.log('teste');
+    }
+}); 
